0938e787a524893b62a62bf2a93c3b45a33cb095
Tommy Roe <T.Roe@soton.ac.uk>
2026-02-26 17:09:51 +0000

    added optical pupming simulation via random walk

--- sim_library/simulation.py
+++ sim_library/simulation.py
@@ -4,11 +4,10 @@ from sim_library.constants import k_eff, kb, m, dR
 from sim_library.sequences import PulseSequence
 from sim_library.hams import gen_ham_free, gen_ham_plus, gen_ham_minus, time_evolve
 
-def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int, basis: np.ndarray, initial_state: np.ndarray) -> tuple[np.ndarray, np.ndarray, dict]:
+def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int, basis: np.ndarray, initial_state: np.ndarray):
     """
     Simulates a sequence of pulses on an initial state for an ensemble of atoms defined by the given temperature.
-    This functions returns an array containing all the possible momentum values as well as an array containing their corresponding weights
-    at every time step.
+    This functions returns the momentum distribution along with the weights (probabilities) corresponding to each momentum state.
 
     Args:
         pulse_seq (PulseSequence): The ordered sequence of pulses to be applied to each atom.
@@ -19,9 +18,11 @@ def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int,
                                        
 
     Returns:
-        tuple: A tuple containing four elements:
-            - mom_dist (numpy.ndarray): 1D array of all possible momentum values (of size N x basis length).
-            - state_fractions (numpy.ndarray): 2D array of the weights (probabilities) of the mom_dist values at every time step (of shape (time steps, N x basis length)).
+        tuple: A tuple containing five elements:
+            - mom_dist (numpy.ndarray): 1D array of all possible momentum values (of size N*basis length). Use this to plot a histogram.
+            - state_fractions (numpy.ndarray): 2D array of the weights (probabilities) of the mom_dist values at every time step (of shape (time steps, N*basis length)). Use this to plot a histogram.
+            - mom_dist_grid (numpy.ndarray): 2D array of all possible momentum values (of shape (basis length, N)).
+            - state_fractions_grid (numpy.ndarray): 3D array of the weights (probabilities) of the mom_dist_grid values at every time step (of shape (time steps, basis length, N)).
             - rng_state (dict): The state of the numpy random number generator used to generate the initial velocity distribution (containing the seed).
 
     Raises:
@@ -45,11 +46,11 @@ def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int,
 
     n_steps = pulse_seq.get_n_steps()
 
-    data = np.zeros((n_steps, len(basis), len(atom_veloc)))
+    state_fractions_grid = np.zeros((n_steps, len(basis), len(atom_veloc)))
 
     state_vec = initial_state
     square = np.abs(state_vec)**2
-    data[0,:,:] = square[:, np.newaxis] # populate first time step with initial state for every atom
+    state_fractions_grid[0,:,:] = square[:, np.newaxis] # populate first time step with initial state for every atom
 
     for v in range(len(atom_veloc)):
         state_vec = initial_state # reset back to initial state for each atom
@@ -61,7 +62,7 @@ def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int,
         for t in range(1, n_steps):
             state_vec = time_evolve(times[t]-times[t-1], hams[t-1]) @ state_vec # hams is one shorter than times
             square = np.abs(state_vec)**2
-            data[t,:,v] = square
+            state_fractions_grid[t,:,v] = square
         
         
     # remember this is still relative to the initial state    
@@ -72,14 +73,83 @@ def simulate_pulses_p_dist(pulse_seq: PulseSequence, Temp: float, no_atoms: int,
 
     # this is a list of all the possible momentum states, 
     # this is fixed and just depends on the initial distribution and the basis
-    mom_dist_tiled = init_mom_dist_tiled + (hbar*k_eff*basis_tiled)
-    mom_dist = np.ravel(mom_dist_tiled) 
+    mom_dist_grid = init_mom_dist_tiled + (hbar*k_eff*basis_tiled)
+    mom_dist = np.ravel(mom_dist_grid) 
 
     # this all of the probabilities or weights at each time step
     # the rows line up with mom_dist, then the the column dimension is the time
-    state_fractions = data.reshape(data.shape[0],-1)
+    state_fractions = state_fractions_grid.reshape(state_fractions_grid.shape[0],-1)
     
-    return mom_dist, state_fractions, rng_state
+    return mom_dist, state_fractions, mom_dist_grid, state_fractions_grid, rng_state
+
+def simulate_pulses_p_dist_custom(pulse_seq: PulseSequence, init_mom_dist: np.ndarray, basis: np.ndarray, initial_state: np.ndarray):
+    """
+    Simulates a sequence of pulses on an initial state for an ensemble of atoms with a given momentum distribution.
+    This functions returns the momentum distribution along with the weights (probabilities) corresponding to each momentum state.
+
+    Args:
+        pulse_seq (PulseSequence): The ordered sequence of pulses to be applied to each atom.
+        init_mom_dist (numpy.ndarray): 1D array of initial momentum distribution (of size N).
+        basis (numpy.ndarray): The 1D array of momentum basis states (in integers of hbar*k_eff).
+        initial_state (numpy.ndarray): The initial momentum state vector of a single atom (elements are np.complex128). 
+                                       
+
+    Returns:
+        tuple: A tuple containing four elements:
+            - mom_dist (numpy.ndarray): 1D array of all possible momentum values (of size N*basis length). Use this to plot a histogram.
+            - state_fractions (numpy.ndarray): 2D array of the weights (probabilities) of the mom_dist values at every time step (of shape (time steps, N*basis length)). Use this to plot a histogram.
+            - mom_dist_grid (numpy.ndarray): 2D array of all possible momentum values (of shape (basis length, N)).
+            - state_fractions_grid (numpy.ndarray): 3D array of the weights (probabilities) of the mom_dist_grid values at every time step (of shape (time steps, basis length, N)).
+
+    Raises:
+        TypeError: If initial_state is not a complex np.ndarray.
+        ValueError: If list of pulses is empty.
+    """
+    # Checks the initial state vector is a complex np.ndarray
+    if not np.issubdtype(initial_state.dtype, np.complexfloating):
+        raise TypeError(f"The 'initial_state' array must have a complex dtype (e.g., np.complex128), but received {initial_state.dtype}.")
+
+    if len(pulse_seq.pulses) <= 0:
+        raise ValueError("List of pulses is empty")
+
+    atom_veloc = init_mom_dist/m
+
+    n_steps = pulse_seq.get_n_steps()
+
+    state_fractions_grid = np.zeros((n_steps, len(basis), len(atom_veloc)))
+
+    state_vec = initial_state
+    square = np.abs(state_vec)**2
+    state_fractions_grid[0,:,:] = square[:, np.newaxis] # populate first time step with initial state for every atom
+
+    for v in range(len(atom_veloc)):
+        state_vec = initial_state # reset back to initial state for each atom
+
+        pulse_seq.gen_hams(basis= basis, doppler_shift= k_eff*atom_veloc[v])
+        hams = pulse_seq.hams
+        times = pulse_seq.times
+        
+        for t in range(1, n_steps):
+            state_vec = time_evolve(times[t]-times[t-1], hams[t-1]) @ state_vec # hams is one shorter than times
+            square = np.abs(state_vec)**2
+            state_fractions_grid[t,:,v] = square
+        
+        
+    # remember this is still relative to the initial state    
+    init_mom_dist_tiled = np.tile(init_mom_dist, (len(basis),1))
+
+    basis_tiled = np.transpose(np.tile(basis, (len(atom_veloc),1)))
+
+    # this is a list of all the possible momentum states, 
+    # this is fixed and just depends on the initial distribution and the basis
+    mom_dist_grid = init_mom_dist_tiled + (hbar*k_eff*basis_tiled)
+    mom_dist = np.ravel(mom_dist_grid) 
+
+    # this all of the probabilities or weights at each time step
+    # the rows line up with mom_dist, then the the column dimension is the time
+    state_fractions = state_fractions_grid.reshape(state_fractions_grid.shape[0],-1)
+    
+    return mom_dist, state_fractions, mom_dist_grid, state_fractions_grid
 
 def simulate_pulses_single_atom(pulse_seq: PulseSequence, basis: np.ndarray, initial_state: np.ndarray, d_shift: float = 0) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -124,3 +194,61 @@ def simulate_pulses_single_atom(pulse_seq: PulseSequence, basis: np.ndarray, ini
         wave_func[:,i] = state_vec
 
     return np.transpose(wave_func), times
+
+def p_shift_random_walk(ground_state_ratio: float):
+    """Performs a random walk to simulate how many photon absorptions and spontaneous emissions before an atom is pumped to a dark state.
+
+    Args:
+        ground_state_ratio (float): The probability of the atom decaying to the ground (dark) state after being excited. Calculated from transition strengths.
+
+    Returns:
+        tuple: A tuple containing:
+            - p (float): The total accumulated momentum shift.
+            - n (int): The total number of photon scatters that occur.
+    """
+    p=0
+    n=0
+    walking = True
+    while walking:
+        p += np.random.choice([-1,1])
+        p += np.random.uniform(-1,1)
+        n += 1
+        a = np.random.random()
+        if a < ground_state_ratio:
+            walking = False
+    return p, n
+ 
+    
+def simulate_optical_pumping(basis: np.ndarray, init_mom_dist_grid: np.ndarray, state_fracs_grid: np.ndarray, pumping_route: str = 'f3'):
+    """Simulates the effect of optical pumping on the momentum distribution of an atomic ensemble.
+
+    Args:
+        basis (np.ndarray): The 1D array of momentum basis states (in integers of hbar*k_eff).
+        init_mom_dist_grid (np.ndarray): 2D array of the initial momentum distribution 
+            grid (of shape (basis length, N)).
+        state_fracs_grid (np.ndarray): 2D array of state probabilities corresponding 
+            to the basis states for each atom (of shape (basis length, N)).
+        pumping_route (str, optional): Upper hyperfine state that the pumping route takes. Options are 'f3' (default) and 'f2', corresponding to F'=3 and F'=2.
+
+    Returns:
+        np.ndarray: 1D array of the new momentum distribution for the atom ensemble (of length N).
+    """
+    n_atoms = init_mom_dist_grid.shape[1]
+    new_mom_dist = np.zeros(n_atoms)
+    pumping_momentum = hbar*k_eff*0.5 #assume transition energy is half of raman transition
+
+    if pumping_route == 'f3':
+        ground_state_ratio = 0.528
+    else:
+        ground_state_ratio = 0.831
+
+    for i in range(n_atoms):
+        basis_index = np.random.choice(a=np.arange(len(basis)), p=state_fracs_grid[:,i])
+        if basis[basis_index] % 2 == 0:
+            new_mom_dist[i] += init_mom_dist_grid[basis_index, i]
+        else:
+            p_shift, _ = p_shift_random_walk(ground_state_ratio=ground_state_ratio)
+            p_shift = p_shift*pumping_momentum
+            new_mom_dist[i] += init_mom_dist_grid[basis_index, i] + p_shift
+            
+    return new_mom_dist
